d453ce9c4b39e1315a78e0ddad22cd7a26bcb73b
shivani97 <shivani.agrawal76@gmail.com>
2020-09-18 11:16:44 +0530

    Adding Screenshots

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-var randomNumber1 = Math.floor(Math.random()*5)+1;
-var randomImage1 = "images/dice"+randomNumber1+".png";
+var randomNumber1 = Math.floor(Math.random() * 5) + 1;
+var randomImage1 = "images/dice" + randomNumber1 + ".png";
 document.querySelector(".img1").setAttribute("src", randomImage1);
-var randomNumber2 = Math.floor(Math.random()*6)+1;
-var randomImage2 = "images/dice"+randomNumber2+".png";
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImage2 = "images/dice" + randomNumber2 + ".png";
 document.querySelector(".img2").setAttribute("src", randomImage2);
-if(randomNumber1>randomNumber2)
-{
-    document.querySelector("h1").innerHTML="Player1 wins";
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player1 wins";
 }
-else if(randomNumber1===randomNumber2)
-{
-    document.querySelector("h1").innerHTML="OOPS! Match is tie.";
+else if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "OOPS! Match is tie.";
 }
-else
-{
-    document.querySelector("h1").innerHTML="Player2 wins";
+else {
+    document.querySelector("h1").innerHTML = "Player2 wins";
 }
